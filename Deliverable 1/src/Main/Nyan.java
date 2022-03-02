84ae92f185c14611b0856a60e3ae1f39b3f032f5
Colbawa <>
2022-03-02 12:24:54 -0500

    Preet

---
+++ Deliverable 1/src/Main/Nyan.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package Main;
+
+/**
+ *This class +++ Insert class description here +++
+ *
+ *@author Preetinder Singh
+ */
+public class Nyan {
+
+   
+    public static void main(String[] args) {
+        System.out.println("YEEHAW");
+    }
+
+}
