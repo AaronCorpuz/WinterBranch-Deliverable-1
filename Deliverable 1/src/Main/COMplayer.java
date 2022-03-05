43f4695f5ba509894eb3b47ad6c5b98900c0dfa9
Colbawa <Acer@192.168.2.224>
2022-03-04 22:11:03 -0500

    Code workey? me no know

---
+++ Deliverable 1/src/Main/COMplayer.java
@@ -0,0 +1,51 @@
+package Main;
+import java.util.ArrayList;
+public class COMplayer 
+{
+    ArrayList<Integer> hand = new ArrayList<>(); 
+
+    public ArrayList<Integer> getHand() {
+        return hand;
+    }
+
+    public void setHand(ArrayList<Integer> hand) {
+        this.hand = hand;
+    }
+    
+    public void handAdd(int x)
+    {
+    hand.add(x);
+    }
+    public Integer cardAsk()
+    {
+     int index = (int)(Math.random() * hand.size());
+  
+     System.out.println("Card: " + hand.get(index));
+     return hand.get(index);
+    }
+    public int cardCheck(int y)
+    {
+    int count=0;
+    for(int i=0;i<=hand.size();i++)
+    {
+        if(hand.get(i)==y)
+        {
+            count++;
+        }
+    }
+    return count;
+    }
+    public int cardSelect(int z)
+    {
+    int count=0;
+    for(int i=0;i<=hand.size();i++)
+    {
+        if(hand.get(i)==z)
+        {
+            count++;
+            hand.remove(i);
+        }
+    }
+    return count;
+}
+}
